90528367a22ed452beddce2da7e5aa02daa64576
Dharangan <61000234+Dharangan@users.noreply.github.com>
2020-03-05 17:47:09 +0530

    Update Medium.c

--- Program 05.03.2020/Medium.c
+++ Program 05.03.2020/Medium.c
@@ -1,16 +1,9 @@
-/******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-// Did for two cases
+// Did for three cases
 int main()
 {
     int n,s,p,k,i,j,a=1,b,c,f=0;
@@ -52,6 +45,10 @@ int main()
                 if(f)
                 {
                     break;
+                }
+                else
+                {
+                    printf("NO");
                 }
             }
             break;
